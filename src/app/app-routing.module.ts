287f1ac8391ab05f91f2b2b2aa0e78c38fdecc86
quangbui205 <bmq205999@gmail.com>
2020-09-10 15:19:58 +0700

    add file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./layouts/login/login.component";
+import {MasterComponent} from "./layouts/master/master.component";
+import {DashboardComponent} from "./layouts/dashboard/dashboard.component";
+import {UserListComponent} from "./comporents/users/user-list/user-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',component: LoginComponent},
+  {path:'admin',redirectTo:'admin/dashboard', pathMatch:'full'},
+  {
+    path: 'admin',
+    component: MasterComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {
+        path: 'users',
+        children: [
+          {path: '',component:UserListComponent }
+        ]
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layouts/login/login.component';
+import { MasterComponent } from './layouts/master/master.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { UserListComponent } from './comporents/users/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MasterComponent,
+    NavbarComponent,
+    DashboardComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
